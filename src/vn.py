7ccf012fd9a727f13ba272bbd6f16e6e37a222ea
Stef McCuistion <stef.mccuistion@gmail.com>
2024-12-03 16:13:36 -0600

    Two lines of text can now display

--- src/vn.py
+++ src/vn.py
@@ -26,7 +26,8 @@ def main():
     txt_box = pygame.image.load('img_files/ui_textbox.png')
     txt = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \nut labore et dolore magna aliqua. Ut enim ad minim veniam, \nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     txt = txt.split('\n')
-    txt_surface = font.render(txt[0], False, (255, 255, 255))
+    txt_line1 = font.render(txt[0], False, (255, 255, 255))
+    txt_line2 = font.render(txt[1], False, (255, 255, 255))
 
     while True: 
         for event in pygame.event.get():
@@ -42,7 +43,8 @@ def main():
         screen.blit(background, (0,0))
         sprite.draw(screen)
         screen.blit(txt_box, (0,0))
-        screen.blit(txt_surface, (30, 240))
+        screen.blit(txt_line1, (30, 240))
+        screen.blit(txt_line2, (30, 270))
 
         pygame.display.update()
         clock.tick(60)
