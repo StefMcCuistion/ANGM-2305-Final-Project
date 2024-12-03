c2693e21d9df634edc90e2115f067cfe057e6b64
Stef McCuistion <stef.mccuistion@gmail.com>
2024-12-03 15:06:53 -0600

    Sprite is now called and appears on screen

--- src/vn.py
+++ src/vn.py
@@ -2,20 +2,21 @@ import pygame
 from sys import exit
 
 class Sprite():
-    def __init__(self, name, dir, res):
+    def __init__(self, name, dir=1, res=(800,400)):
         self.name = name
         self.dir = dir
         self.res = res
 
-    def draw(self, surface):
-        surf = pygame.image.load(f"img_files/spr_{self.name}")
-        return surf
+    def draw(self, screen):
+        surf = pygame.image.load(f"img_files/spr_{self.name}.png")
+        screen.blit(surf, (0,0))
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((800,400))
     pygame.display.set_caption('Visual Novel')
     clock = pygame.time.Clock()
+    sprite = Sprite(name="protag")
 
 
     background = pygame.image.load('img_files/bg_placeholder.png')
@@ -27,6 +28,7 @@ def main():
                 exit()
 
         screen.blit(background, (0,0))
+        sprite.draw(screen)
 
         pygame.display.update()
         clock.tick(60)
