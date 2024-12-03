a82a48501cb7d176a1eeb3ea940a9f251f07e694
Stef McCuistion <stef.mccuistion@gmail.com>
2024-12-03 14:59:30 -0600

    Moved code to main function

--- src/vn.py
+++ src/vn.py
@@ -11,20 +11,24 @@ class Sprite():
         surf = pygame.image.load(f"img_files/spr_{self.name}")
         return surf
 
-pygame.init()
-screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Visual Novel')
-clock = pygame.time.Clock()
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800,400))
+    pygame.display.set_caption('Visual Novel')
+    clock = pygame.time.Clock()
 
-background = pygame.image.load('img_files/bg.png')
+    background = pygame.image.load('img_files/bg.png')
 
-while True: 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+    while True: 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
 
-    screen.blit(background, (0,0))
+        screen.blit(background, (0,0))
 
-    pygame.display.update()
-    clock.tick(60)
+        pygame.display.update()
+        clock.tick(60)
+
+if __name__ == "__main__":
+    main()
