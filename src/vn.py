48ceeac88682dc000155590d2ff694ce2948652e
Stef McCuistion <stef.mccuistion@gmail.com>
2024-12-03 16:11:11 -0600

    Text is now split with \n, and currently only the first line renders

--- src/vn.py
+++ src/vn.py
@@ -24,9 +24,9 @@ def main():
 
     background = pygame.image.load('img_files/bg_placeholder.png')
     txt_box = pygame.image.load('img_files/ui_textbox.png')
-    txt_surface = font.render(
-                               'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                               False, (255, 255, 255))
+    txt = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \nut labore et dolore magna aliqua. Ut enim ad minim veniam, \nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    txt = txt.split('\n')
+    txt_surface = font.render(txt[0], False, (255, 255, 255))
 
     while True: 
         for event in pygame.event.get():
