844e914146cfebeb5055ac4be7a8ed90e08c8983
Stef McCuistion <stef.mccuistion@gmail.com>
2024-12-06 22:02:03 -0600

    Music and sfx buttons now appear, not yet functional nor positioned correctly.

--- src/vn.py
+++ src/vn.py
@@ -49,7 +49,10 @@ class Button():
             else:
                 self.img = pygame.image.load(f"img_files/ui_{self.name}_selected_{res[0]}x{res[1]}.png")
         else:
-            self.img = pygame.image.load(f"img_files/ui_{self.name}_unselected_{res[0]}x{res[1]}.png")
+            if self.binary:
+                self.img = pygame.image.load(f"img_files/ui_{self.name}_{self.on_or_off}_unselected_{res[0]}x{res[1]}.png")
+            else:
+                self.img = pygame.image.load(f"img_files/ui_{self.name}_unselected_{res[0]}x{res[1]}.png")
 
 
 def display_dialogue(screen, txt, font, res):
@@ -202,8 +205,8 @@ def settings_menu(res):
     sfx_button_surface = pygame.image.load(f"img_files/ui_sfx_on_unselected_{res[0]}x{res[1]}.png")
 
     return_button = Button(name="return", img=return_button_surface, x=(res[0]*0.5), y=(res[1]*0.8), resolution=(1040,820))
-    music_button = Button(name="music", img=music_button_surface, x=(res[0]*0.5), y=(res[1]*0.8), resolution=(1040,820))
-    sfx_button = Button(name="sfx", img=sfx_button_surface, x=(res[0]*0.5), y=(res[1]*0.8), resolution=(1040,820))
+    music_button = Button(name="music", img=music_button_surface, x=(res[0]*0.7), y=(res[1]*0.6), binary = 1, resolution=(1040,820))
+    sfx_button = Button(name="sfx", img=sfx_button_surface, x=(res[0]*0.7), y=(res[1]*0.4), binary = 1, resolution=(1040,820))
 
     screen.fill('black')
     bg = pygame.image.load(f"img_files/ui_settings_{res[0]}x{res[1]}.png")
@@ -221,7 +224,11 @@ def settings_menu(res):
                     break
         
         return_button.update(screen)
+        music_button.update(screen)
+        sfx_button.update(screen)
         return_button.change_appearance(pygame.mouse.get_pos(), res)
+        music_button.change_appearance(pygame.mouse.get_pos(), res)
+        sfx_button.change_appearance(pygame.mouse.get_pos(), res)
         
         pygame.display.update()
         clock.tick(60)
